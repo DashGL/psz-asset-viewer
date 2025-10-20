3a8d33c7e40aa20740d36676025f72c079fdcf4d
kion@dashgl.com <kion@dashgl.com>
2025-10-20 10:59:21 -0500

    deploy args

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dashgl.github.io',
+  base: '/psz-asset-viewer',
   integrations: [react()],
   vite: {
     server: {
